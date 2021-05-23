e8a32f3537d93d0bf93c904436778e22e5e8617d
Michal Jirku <box@wejn.org>
2021-05-23 23:28:52 +0200

    First shot at balance

--- src/Pages/Balance.elm
+++ src/Pages/Balance.elm
@@ -7,40 +7,68 @@ import Element.Font as Font
 import Element.Background as Background
 import Html
 import Html.Events
+import Http
 import Page
 import Request exposing (Request)
 import Shared
 import Storage exposing (Storage)
+import Time
 import View exposing (View)
 
 page : Shared.Model -> Request -> Page.With Model Msg
 page shared req =
     Page.element
-        { init = init req
+        { init = init req shared.storage
         , update = update shared.storage
         , view = view shared.storage
         , subscriptions = subscriptions
         }
 
-type alias Model =
-    { menu : Element Msg }
+type State
+    = Loading
+    | Success String
+    | Failure
 
-init : Request -> ( Model, Cmd Msg )
-init req =
-    ( { menu = Shared.menuGen req}, Cmd.none )
+type alias Model =
+    { menu : Element Msg
+    , state : State
+    , balance : String
+    , balanceTs : Time.Posix }
+
+init : Request -> Storage -> ( Model, Cmd Msg )
+init req storage =
+    ( { menu = Shared.menuGen req
+      , state = Loading
+      , balance = storage.balance
+      , balanceTs = storage.balanceTs
+      }, loadBalance storage )
 
 type Msg
     = None
+    | GotBalance (Result Http.Error String)
 
 
 update : Storage -> Msg -> Model -> ( Model, Cmd Msg )
 update storage msg model =
     case msg of
         None -> ( model , Cmd.none )
+        GotBalance result ->
+            case result of
+                Ok fullText ->
+                    ({ model | state = (Success fullText), balance = fullText }, Cmd.none) -- FIXME: update refresh TS
+                Err _ ->
+                    ({ model | state = Failure }, Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
+
+balanceForModel : Model -> String
+balanceForModel m =
+    case m.state of
+        Loading -> "Loading ..."
+        Success _ -> m.balance
+        Failure -> "Failed to fetch."
 
 view : Storage -> Model -> View Msg
 view storage m =
@@ -50,7 +78,13 @@ view storage m =
         el [ centerX , centerY, padding 50 ] <|
             column [ spacing 40 ]
             [ paragraph
-              []
-              [ text "Here be balance." ]
+              [Font.size 48]
+              [ text (balanceForModel m) ]
             ]
     }
+
+loadBalance : Storage -> Cmd Msg
+loadBalance storage =
+    Http.get
+        { url = Shared.apiUrlFromCreds storage "balance" -- FIXME: mod the time, to force refresh
+        , expect = Http.expectString GotBalance }
