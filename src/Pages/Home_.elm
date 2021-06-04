81fa4439d6b002550bdcb540de1fadef35c7a08a
Michal Jirku <box@wejn.org>
2021-06-04 15:49:48 +0200

    Finish homepage (link to a blogpost, linkify credit)

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -67,7 +67,7 @@ view : Request -> Shared.Model -> Model -> View Msg
 view req shared m =
     let
         odorikApi = "https://www.odorik.cz/w/api"
-        blogPost = "https://wejn.org/FIXME-FIXME-FIXME/" -- FIXME
+        blogPost = "https://wejn.org/2021/06/making-ios-custom-dialer-app-with-shortcuts-and-elm-spa/"
         shortcut = "https://www.icloud.com/shortcuts/676a2e2fdb2b40e2ba5c1c182238d54d"
         source = "https://github.com/wejn/odorik-spa"
         monospaced = (\x -> el [ Font.family [ Font.monospace ], Background.color <| rgb255 242 242 242 ] x)
@@ -156,6 +156,8 @@ view req shared m =
             , paragraph [ Font.size 24, Font.bold ]
                 [ text "Credit" ]
             , paragraph [ Font.justify ]
-                [ text "© 2021 Michal Jirků"
+                [ text "© 2021 "
+                , newTabLink Attr.link { label = text "Michal Jirků", url = "https://wejn.org/" }
+                , text "."
                 ]
         ]
